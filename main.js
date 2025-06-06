3c63cb951ae8e01e99d23d0a2d4ae3f90987290b
Whyno798 <79536860663r@gmail.com>
2025-06-06 10:38:09 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,38 +1,44 @@
 function setupEmailCopy() {
     const emailBtn = document.getElementById('copyEmailBtn');
+    const email = '79536860663@mail.ru';
 
     if (!emailBtn) return;
 
-    emailBtn.addEventListener('click', () => {
-        const email = '79536860663@mail.ru';
-
-        navigator.clipboard.writeText(email)
-            .then(() => {
-                emailBtn.classList.add('copied');
-                emailBtn.innerHTML = '<i class="fas fa-check"</i> Скопировано!';
-
-                setTimeout(() => {
-                    emailBtn.classList.remove('copied');
-                    emailBtn.innerHTML = '<i class="fas fa-copy"</i> Скопировать email';
-                }, 2000);
-            })
-            .catch(err => {
-                console.error('Ошибка при копировании:', err);
-                const textArea = document.createElement('textarea');
-                textArea.value = email;
-                document.body.appendChild(textArea);
-                textArea.select();
-                document.execCommand('copy');
-                document.body.removeChild(textArea);
-
-                emailBtn.innerHTML = '<i class="fas fa-check"></i> Скопировано!';
-                setTimeout(() => {
-                    emailBtn.innerHTML = '<i class="fas fa-copy"</i> Скопировать email';
-                }, 2000);
-            });
+    emailBtn.addEventListener('click', async () => {
+        try {
+            await navigator.clipboard.writeText(email);
+            emailBtn.innerHTML = '<i class="fas fa-check"</i> Скопировано!';
+            emailBtn.style.backgroundColor = '#2ecc71';
+            setTimeout(() => {
+                emailBtn.innerHTML = '<i class="fas fa-copy"</i> Скопировать email';
+                emailBtn.style.backgroundColor = '';
+            }, 2000);
+        } catch (err) {
+            console.error('Ошибка', err);
+            emailBtn.textContent = 'Ошибка :(';
+        }
     });
-};
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     setupEmailCopy();
 });
+
+function initTheme() {
+    const themeBtn = document.getElementById('themeToogle');
+
+    if (localStorage.getItem('theme') === 'dark') {
+        document.body.classList.add('dark-theme');
+        themeBtn.innerHTML = '<i class="fas fa-sun"></i>';
+    }
+
+    themeBtn.addEventListener('click', () => {
+        document.body.classList.toogle('dark-theme');
+        const icon = themeBtn.querySelector('i');
+        icon.classList.toogle('fa-moon');
+        icon.classList.toogle('fa-sun');
+
+        localStorage.setItem('theme',
+            document.body.classList.contains('dark-theme') ? 'dark' : 'light');
+    });
+}
